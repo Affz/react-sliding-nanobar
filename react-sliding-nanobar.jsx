a17650a81020a4d51d59ce68066be8d1d27dd656
ashish <ashish@riverus.in>
2019-02-24 14:44:28 +0530

    props added for options

--- react-sliding-nanobar.jsx
+++ react-sliding-nanobar.jsx
@@ -1,8 +1,31 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import './loader.css';
 
-export default function ReactInfiniteNanobar() {
-    return (
-        <div className="nanobar-loader" />
-    );
+const propTypes = {
+   options: {
+       color: PropTypes.string
+   }
 }
+
+const defaultProps = {
+    options: {
+        color: '#91cde2'
+    }
+}
+class ReactInfiniteNanobar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        var { options } = this.props;
+        return (
+            <div style={{backgroundColor: options.color}} className="nanobar-loader" />
+        );
+    }
+}
+
+ReactInfiniteNanobar.propTypes = propTypes;
+ReactInfiniteNanobar.defaultProps = defaultProps;
+
+export default ReactInfiniteNanobar;
